d190ad45ee983de23614bcd98787cefd9eaca62e
galyna <galaibidag@gmail.com>
2025-03-29 00:15:49 +0200

    add updatework with correction

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,13 +1,15 @@
-import MagicButton from '@/app/components/magic-button';
+// import MagicButton from '@/app/components/magic-button';
 import React from 'react';
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
-    </main>
-  );
+  return <Header>Dashboard</Header>;
+  // (
+  //   <main>
+  //     <h1 className="text-xl">Dashboard Page</h1>
+  //     {/* <MagicButton /> */}
+  //   </main>
+  // );
 }
